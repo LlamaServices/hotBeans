a2d135bc4c208089990a30877eac37ee4de6639e
crackMaker <aleksa.andric064@gmail.com>
2022-10-15 17:11:28 +0200

    Added score count for D, removed old D

--- D.py
+++ D.py
@@ -1,28 +1,17 @@
-# Ovo resenje je ocajno napisano, bolela me je glava kad sam ga pisao tako da
-# *Ne gledajte ovo, vec D2*
-# 48 poena 
-
+# 100 Poena
 instr = input()
 outstr = ''
-last = ''
-if instr[0] == instr[-1]:
-    print(instr[0] + str(instr.count(instr[0])))
-else:
-    instr = list(instr)
-    while len(instr):
-        if instr[0] == instr[-1]:
-            outstr += instr[0]
-            if instr.count(instr[0]) > 1:
-                outstr += str(instr.count(instr[0]))
-            break
-        c = instr[0]
-        outstr += c
-        if instr.count(c) > 1:
-            outstr += str(instr.count(c))
-        for x in instr:
-            try:
-                instr.remove(c)
-            except:
-                pass
+
+last = instr[0]
+outstr += last
+if instr.count(last) > 1:
+    outstr += str(instr.count(last))
+
+for x in instr:
+    if x != last:
+        outstr += x
+        if instr.count(x) > 1:
+            outstr += str(instr.count(x))
+        last = x
 
 print(outstr)
