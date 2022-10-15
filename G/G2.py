8548c63e2aa11309c4f1abc0ae541cdfd7cb75f9
crackMaker <aleksa.andric064@gmail.com>
2022-10-15 17:12:03 +0200

    Removed all old versions

--- G/G2.py
+++
@@ -1,9 +0,0 @@
-l1 = [int(x) for x in input().split()]
-l2 = [int(x) for x in input().split()]
-b = s = list()
-if len(l1)>len(l2): b, s = l1, l2
-else: s, b = l1, l2
-meow = set(b).difference(s)
-for x in meow:
-    break
-print(x)
--- G/G3.py
+++
@@ -1,9 +0,0 @@
-l1 = [int(x) for x in input().split()]
-l2 = [int(x) for x in input().split()]
-l = l1+l2
-
-for x in l:
-    if l.count(x) > 1: pass
-    else:
-        print(x)
-        break
