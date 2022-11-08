16f08b0ac61d9f9dc69441fc7b0487c8223d2cac
crackMaker <aleksa.andric064@gmail.com>
2022-11-08 23:39:00 +0100

    Misc updates

--- Petlja_Takmicanje/J.py
+++ Petlja_Takmicanje/J.py
@@ -1,11 +1,16 @@
-# 58 poena
-starting_number = int(input())
-number = starting_number
-while True:
-    number = sum([int(x)**2 for x in str(number)])
-    if number == starting_number:
-        print("ne")
-        break
-    if number == 1:
-        print("da")
-        break
+# 100 poena
+from time import time as t
+def meow(num):
+    def sumDig(num):
+        return sum([int(x)**2 for x in str(num)])
+
+    starting_num = num
+    c = 0
+    while(True):
+        num = sumDig(num)
+        if num == starting_num: return 'ne'
+        if num == 1: return 'da'
+        c += 1
+        if c > 100: return 'ne'
+
+print(meow(int(input())))
--- Petlja_Takmicanje/M.py
+++ Petlja_Takmicanje/M.py
@@ -3,5 +3,7 @@ from statistics import median
 list1 = [int(x) for x in input().split()]
 list2 = [int(x) for x in input().split()]
 flist = list1 + list2
+del list1
+del list2
 flist.sort()
 print(median(flist))
