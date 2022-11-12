60168ac7f04d9a4968ea4899d75f6c106bc666dd
crackMaker <aleksa.andric064@gmail.com>
2022-11-12 22:05:23 +0100

    Started solving workbook

---
+++ Metodicka_Zbirka_Zadataka/Elementarna_celobrojna_i_realna_aritmetika/Geometriske_formule/5_Rastojanje_tacaka.py
@@ -0,0 +1,7 @@
+# https://petlja.org/biblioteka/r/Zbirka-python/rastojanje_tacaka
+from math import sqrt
+x = int(input())
+y = int(input())
+a = int(input())
+b = int(input())
+print(round(sqrt( ((x-a)**2) + ((y-b)**2) ), 5))
