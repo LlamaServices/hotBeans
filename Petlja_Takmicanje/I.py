4892711e356f13477a2bdffc586d560c1a887a6d
crackMaker <aleksa.andric064@gmail.com>
2022-11-17 15:47:45 +0100

    Added more solutions, fixed I.py for 92 points

--- Petlja_Takmicanje/I.py
+++ Petlja_Takmicanje/I.py
@@ -1,19 +1,26 @@
-# 38 poena
-def meow(niz):
-    lenght = 1
-    maxi = 1
-    for x in range(1, len(niz)):
-        if niz[x-1] > niz[x]:
-            lenght+=1
-        else:
-            if lenght > maxi:
-                maxi = lenght
-            lenght = 1
-        if niz[x] == niz[-1]:
-            if lenght > maxi:
-                maxi = lenght
-            lenght = 1
-    return maxi
+# 92 poena
+def meow(arr: list) -> int:
+    if len(set(arr)) == 1:
+        return 1
+    
+    NAJduz = 1
+    duz = 1
+    for i in range(1, len(arr)):
+        if arr[i] < arr[i-1]:
+            duz += 1
+        
+        if arr[i] > arr[i-1] and duz > NAJduz:
+            NAJduz = duz
+            duz = 1
 
-niz = [int(x) for x in input().split()]
-print(meow(niz))
+        if arr[i] > arr[i-1] and not duz > NAJduz:
+            duz = 1
+
+        if i == (len(arr) - 1):
+            if duz > NAJduz:
+                NAJduz = duz
+
+    return NAJduz
+
+#print(meow([int(x) for x in input().split()]))
+print(meow( [2,2,2,2,1] ))
--- Stariji_Kolokviumi/2016/Prvi_kolok/2.py
+++ Stariji_Kolokviumi/2016/Prvi_kolok/2.py
@@ -1,11 +1,11 @@
-from math import sqrt
+from math import log
 
 def meow(osnova: int, br: int) -> int:
     if not osnova > 1:
         raise ValueError
     if not br > 0:
         raise ValueError
-    lower = higher = round(sqrt(br))
+    lower = higher = round(log(br, osnova))
     d = {
         abs(osnova**(lower-1)-br): lower-1,
         abs(osnova**(higher+1)-br): higher+1,
@@ -13,4 +13,4 @@ def meow(osnova: int, br: int) -> int:
     }
     return d.get(min(d.keys()))
 
-print(meow(4, 12))
+print(meow(4, 1))
